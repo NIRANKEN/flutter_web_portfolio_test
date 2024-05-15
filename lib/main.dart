7535577de158ff30bb8ba98de65d065a635fd439
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2024-05-15 15:55:31 +0900

    fix: remove unuse firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_portfolio_test/constants.dart';
 import 'my_home_page.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
 
   // This widget is the root of your application.
   @override
